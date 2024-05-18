9b4e14d9eee193892554e989d008433704a650da
CYP152N1 <87903303+CYP152N1@users.noreply.github.com>
2024-05-18 12:10:12 +0900

    Add files via upload

--- gen.sh
+++ gen.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+RFDIFFUSION_PATH=/home/mdonoda/data/RFdiffusion/RFdiffusion
+PROTEINMPNN_PATH=/home/mdonoda/data/RFdiffusion/ProteinMPNN
+GENSH_PATH=/home/mdonoda/data/RFdiffusion/Gensh
+RFDIFF_num_designs=4
+MPNN_num_seq=5
 
 # このスクリプトは、input_pdbのパスを引数として受け取ります。
 if [[ -z "$1" ]]; then
@@ -24,7 +29,7 @@ output_pref="$(pwd)/output/$pdb_prefix/RFdiffusion/$current_date/$pdb_prefix"
 output_dir="$(pwd)/output/$pdb_prefix/RFdiffusion/$current_date"
 
 # Pythonスクリプトを実行し、出力を変数に格納します。
-output=$(python3 input_recog.py "$input_pdb_path")
+output=$(python3 "${GENSH_PATH}"/input_recog.py "$input_pdb_path")
 if [ $? -ne 0 ]; then
     echo "Error: Python script execution failed."
     exit 1
@@ -38,7 +43,7 @@ cat <<EOF > run_inference.sh
 # conda info -e等で読み込むconda環境の名前を確認してください。
 # 環境構築がうまく行かない場合は参照: https://github.com/RosettaCommons/RFdiffusion/issues/14
 
-/home/mdonoda/data/RFdiffusion/RFdiffusion/scripts/run_inference.py inference.output_prefix=$output_pref 'contigmap.contigs=[$output]' inference.input_pdb=$input_pdb_path inference.num_designs=100
+${RFDIFFUSION_PATH}/scripts/run_inference.py inference.output_prefix=$output_pref 'contigmap.contigs=[$output]' inference.input_pdb=$input_pdb_path inference.num_designs=$RFDIFF_num_designs
 EOF
 
 # 生成したシェルスクリプトを実行可能にします。
@@ -73,7 +78,7 @@ do
 
         # Run Python script to prepare data for ProteinMPNN
         echo "Preparing data for ProteinMPNN with: $dir_name/$(basename "$file")"
-        fixed_positions=$(python3 MPNN-prep.py "$dir_name/$(basename "$file")")
+        fixed_positions=$(python3 "${GENSH_PATH}"/MPNN-prep.py "$dir_name/$(basename "$file")")
 
         # Generate and execute a script for ProteinMPNN
         protein_mpnn_output_dir="$(pwd)/output/$pdb_prefix/ProteinMPNN/$current_date/$(basename "$dir_name")"
@@ -95,15 +100,15 @@ chains_to_design="A"
 fixed_positions="$fixed_positions"
 
 # Run ProteinMPNN helper scripts and main script
-python /home/mdonoda/data/RFdiffusion/ProteinMPNN/helper_scripts/parse_multiple_chains.py --input_path=\$folder_with_pdbs --output_path=\$path_for_parsed_chains
-python /home/mdonoda/data/RFdiffusion/ProteinMPNN/helper_scripts/assign_fixed_chains.py --input_path=\$path_for_parsed_chains --output_path=\$path_for_assigned_chains --chain_list "\$chains_to_design"
-python /home/mdonoda/data/RFdiffusion/ProteinMPNN/helper_scripts/make_fixed_positions_dict.py --input_path=\$path_for_parsed_chains --output_path=\$path_for_fixed_positions --chain_list "\$chains_to_design" --position_list "\$fixed_positions"
+python ${PROTEINMPNN_PATH}//helper_scripts/parse_multiple_chains.py --input_path=\$folder_with_pdbs --output_path=\$path_for_parsed_chains
+python ${PROTEINMPNN_PATH}//helper_scripts/assign_fixed_chains.py --input_path=\$path_for_parsed_chains --output_path=\$path_for_assigned_chains --chain_list "\$chains_to_design"
+python ${PROTEINMPNN_PATH}/helper_scripts/make_fixed_positions_dict.py --input_path=\$path_for_parsed_chains --output_path=\$path_for_fixed_positions --chain_list "\$chains_to_design" --position_list "\$fixed_positions"
 python /home/mdonoda/data/RFdiffusion/ProteinMPNN/protein_mpnn_run.py \
         --jsonl_path \$path_for_parsed_chains \
         --chain_id_jsonl \$path_for_assigned_chains \
         --fixed_positions_jsonl \$path_for_fixed_positions \
         --out_folder \$output_dir \
-        --num_seq_per_target 100 \
+        --num_seq_per_target $MPNN_num_seq \
         --sampling_temp "0.1" \
         --seed 37 \
         --batch_size 1
@@ -118,3 +123,36 @@ MPNNEOF
     fi
 done
 
+# 出力ディレクトリのパスを設定
+output_mpnn_dir="$(pwd)/output/$pdb_prefix/ProteinMPNN/$current_date"
+fasta_output_dir="$(pwd)/output/$pdb_prefix/fasta"
+pdb_output_dir="$(pwd)/output/$pdb_prefix/pdb"
+# fasta ディレクトリの作成
+mkdir -p "$fasta_output_dir"
+
+# 最後に、ProteinMPNN用に.faファイルを解析し、個別のファイルに分割する
+echo "Processing .fa files for ProteinMPNN in $output_mpnn_dir and saving to $fasta_output_dir..."
+for fa_file in $(find "$output_mpnn_dir" -name '*.fa'); do
+    fa_basename=$(basename "$fa_file" .fa)
+
+    # .faファイルを解析し、各サンプルに対して新しいファイルを作成
+    awk -v out_dir="$fasta_output_dir" -v base_name="$fa_basename" '
+    /^>/ {
+        if (seq) print seq > filename
+        sample = gensub(/^.*sample=([0-9]+).*$/, "\\1", "g", $0)
+        filename = sprintf("%s/%s_seq_%s.fasta", out_dir, base_name, sample)
+        print ">" base_name > filename
+        seq = ""
+        next
+    }
+    {
+        seq = seq $0
+    }
+    END {
+        if (seq) print seq > filename
+    }' "$fa_file"
+done
+
+echo "Fasta files have been prepared and saved to $fasta_output_dir."
+
+colabfold_batch $fasta_output_dir $pdb_output_dir
